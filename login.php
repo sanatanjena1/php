1f021fd338e56996e22fd063601df2dd6014e812
Sanatan Jena <121142950+sanatanjena1@users.noreply.github.com>
2023-02-18 06:05:31 +0530

    pratice
    php use

---
+++ login.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Final Php Project</title>
+	<style>
+	img
+	{
+		height:500px;
+		width:440px;
+		border-top-left-radius:30px;
+		border-bottom-left-radius:30px;
+	}
+	*
+	{
+		padding:0;
+		margin:0;
+		box-sizing:border-box;
+	}
+    body
+	{
+		background:rgb(219,226,226);
+	}
+	.row
+	{
+		background:white;
+		border-radius:30px;
+		box-shadow:12px 12px 22px grey;
+	}
+    .btn1
+	{
+		border:none;
+		outline:none;
+		height:50px;
+		width:100%;
+		background-color:black;
+		color:white;
+		boder-radius:4px;
+		font-weight:bold;
+	}
+	.btn1:hover
+	{
+		background:white;
+		border:10px solid;
+		color:black;
+	}
+		
+	</style>
+	
+	
+  </head>
+  <body>
+  <section class="form my-4 mx-5">
+  <div class="container">
+  <div class="row no-gutters">
+  <div class="col-lg-5" id="img">
+ <img src="https://west.dpsbangalore.edu.in/wp-content/uploads/2022/09/Admission-Banner-1200x600-1-1024x512.png">
+   </div>
+   <div class="col-lg-7 px-5 pt-5">
+   <h1 class="font-weight-bold py-3">Logo</h1>
+  <h4>Sign into ypur account</h4>
+   <form>
+   <div class="form-row">
+    <div class="col-lg-7">
+	<input type="email" placeholder="Email-Address" class="form-control my-3 py-2">
+	</div>
+   </div>
+   
+   <div class="form-row">
+    <div class="col-lg-7">
+	<input type="Password" placeholder="********" class="form-control my-3 py-2">
+	</div>
+   </div>
+   
+   <div class="form-row">
+    <div class="col-lg-7">
+    <button type="button" class="btn1"><a href="">Login</a></button>
+	</div>
+   </div>
+   
+   <a href="#">Forgot password</a>
+   <p>Don't have an account? <a href="#">Register here</a></p>
+   </form>
+
+   </div>
+   </div>
+   </div>
+   </section>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
